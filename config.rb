e7f0a08a4868d0413223c27fcdae3d6e0814ad57
Flaturtha <rich@thelunaticltd.com>
2013-07-09 12:20:49 -0500

    changes

--- config.rb
+++ config.rb
@@ -2,11 +2,11 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/_assets/css"
-sass_dir = "/_assets/sass"
-images_dir = "/_assets/img"
-javascripts_dir = "/_assets/js"
-fonts_dir = "/_assets/fonts"
+css_dir = "/assets/css"
+sass_dir = "/assets/_sass"
+images_dir = "/assets/img"
+javascripts_dir = "/assets/js"
+fonts_dir = "/assets/fonts"
 
 output_style = :nested
 
